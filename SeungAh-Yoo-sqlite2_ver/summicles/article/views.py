e5b71c79b7429f4e23be9260adc1962903e42d27
SEUNGAH YOO <68517303+SeungAh-Yoo99@users.noreply.github.com>
2021-01-27 17:21:58 +0900

    IT/과학 카테고리가 IT일 때나 과학일 때도 나타나게 수정

--- SeungAh-Yoo-sqlite2_ver/summicles/article/views.py
+++ SeungAh-Yoo-sqlite2_ver/summicles/article/views.py
@@ -1,3 +1,81 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.filters import SearchFilter
+from .models import Article
+from .serializers import ArticleSerializer
 
 # Create your views here.
+
+
+class MainAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['category', 'title', 'article_date', 'contents', 'newspaper']
+
+    def get_queryset(self):  # 어떤 데이터를 가져올 것인지 명시
+        return Article.objects.all().order_by('article_date')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class PoliticsAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):  # 어떤 데이터를 가져올 것인지 명시
+        return Article.objects.filter(category='정치').order_by('article_date')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class EconomicAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):  # 어떤 데이터를 가져올 것인지 명시
+        return Article.objects.filter(category='경제').order_by('article_date')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class SocietyAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):  # 어떤 데이터를 가져올 것인지 명시
+        return Article.objects.filter(category='사회').order_by('article_date')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class CultureAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):  # 어떤 데이터를 가져올 것인지 명시
+        return Article.objects.filter(category='문화').order_by('article_date')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class ForeignAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):  # 어떤 데이터를 가져올 것인지 명시
+        return Article.objects.filter(category='국제').order_by('article_date')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class DigitalAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):  # 어떤 데이터를 가져올 것인지 명시
+        return Article.objects.filter(category='IT/과학', category='IT', category='과학').order_by('article_date')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
